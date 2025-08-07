9406705bfeda3141a7500c9f566ad67f074b4ec0
hoai sang <hoai.sang050@gmail.com>
2025-08-07 22:58:14 +0700

    reupdate develop branch

--- src/main/java/com/mentormentee/eAuctionHouse_be/EAuctionHouseBeApplication.java
+++ src/main/java/com/mentormentee/eAuctionHouse_be/EAuctionHouseBeApplication.java
@@ -7,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EAuctionHouseBeApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(EAuctionHouseBeApplication.class, args);
-		System.out.println("mentor-mentee-update");
 	}
-
 }
