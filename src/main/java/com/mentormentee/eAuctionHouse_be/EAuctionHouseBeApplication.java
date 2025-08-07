73cb81fbfa085e0890bdb80f632863fc59fa9c17
hoai sang <hoai.sang050@gmail.com>
2025-08-07 22:38:48 +0700

    initial commit

---
+++ src/main/java/com/mentormentee/eAuctionHouse_be/EAuctionHouseBeApplication.java
@@ -0,0 +1,13 @@
+package com.mentormentee.eAuctionHouse_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EAuctionHouseBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EAuctionHouseBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mentormentee/eAuctionHouse_be/EAuctionHouseBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentormentee.eAuctionHouse_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EAuctionHouseBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
